6b093cb2e82cb8febe5f75e91e13e60dbd20b55e
Green Apple <akshitverma7361@gmail.com>
2022-10-04 18:13:00 +0530

    added few useful methods to the Class

--- handler/economy.py
+++ handler/economy.py
@@ -8,8 +8,14 @@ import typing
 import discord
 
 from handler.database import database
+from handler.errors import (
+    UserHasNotEnoughCoins,
+    NotEnoughMembers,
+    DataDoesNotExists, NotFound
+)
 
 
+# Economy base class, some useful methods that we will use later
 class Economy:
     def __init__(self, Database: database):
         self.database: database = Database
@@ -25,11 +31,7 @@ class Economy:
             condition="""guild_id = $1
             AND user_id = $2"""
         )
-        return round(total_coins, 2)
-
-    # return user items
-    async def getUserItems(self, user: discord.Member):
-        ...
+        return round(total_coins, 2) if total_coins else 0
 
     # add user coins
     async def addUserCoins(self, user: discord.Member, coins: float = 1):
@@ -45,7 +47,7 @@ class Economy:
             UPDATE SET points = COALESCE(economy.points, 0) + $3""",
         )
         total = await self.getUserCoins(user=user)
-        return round(total,2)
+        return round(total, 2)
 
     # remove user coins
     async def removeUserCoins(self, user: discord.Member, coins: float = 1):
@@ -76,7 +78,7 @@ class Economy:
         return round(total, 2)
 
     # give points to other users
-    async def GivePoints(self, author: discord.Member, user: discord.Member , coins:float):
+    async def GivePoints(self, author: discord.Member, user: discord.Member, coins: float):
         author_coins = await self.getUserCoins(author)
         user_coins = await self.getUserCoins(user)
 
@@ -93,12 +95,124 @@ class Economy:
 
         return round(member_total, 2), round(author_total)
 
-    # buy something for a price
-    async def userBuyitems(self, author: discord.Member, price: float):
+    # get all users sorted my maximum
+    async def getAll_users(self, Guild_id: int, Fetch_row: bool = False, Filter: str = ""):
+
+        List = await self.database.Select(
+            Guild_id,
+            table="test.economy",
+            columns="user_id, points" if not Fetch_row else "*",
+            condition="guild_id = $1",
+            return_everything=True,
+            Filter=Filter
+        )
+        return List
+
+    # get stats of a user
+    async def getUser_stats(self, User: discord.Member):
+        """
+        :param User: the User whose stats you want to see
+        :return: User top percentile, user position and likes
+        """
+
+        UserList = await self.getAll_users(User.guild.id)
+        print(UserList)
+        if not UserList or len(UserList) == 0:
+            raise NotEnoughMembers(" this guild leaderboard is currently not available")
+
+        print("12 ", UserList)
+
+        User_position: int = 0
+        User_likes: int = 0
+
+        # Find the user in asynpg List
+        for i in UserList:
+            User_position += 1
+            if User.id == i['user_id']:
+                User_likes = i['points']
+                break
+
+        # check is user position or likes is None
+        if User_likes == 0:
+            raise DataDoesNotExists(User)
+
+        percent = User_position / len(UserList)
+        if percent <= 0.1:
+            top_rank = '1%'
+        elif 0.1 <= percent <= 0.25:
+            top_rank = '10%'
+        elif 0.25 <= percent <= 0.50:
+            top_rank = '25%'
+        elif 0.50 <= percent <= 0.75:
+            top_rank = '75%'
+        else:
+            top_rank = '90%'
+
+        return top_rank, User_position, round(User_likes, 2)
+
+    """Get shop Related stuffs"""
+
+    # get shop items
+    async def GetShopItems(self):
         ...
 
+    # return item with its name
+    async def GetItemNamed(self, item: str):
+        item = await self.database.Select(
+            item,
+            return_everything=True,
+            table="test.shop",
+            condition="items = $1",
+            columns="items,cost"
+        )
+        if not item:
+            raise NotFound("this item doesnt exist")
+        return item
 
-class UserHasNotEnoughCoins(Exception):
-    def __init__(self, message: str = " user already has no coins"):
-        self.message = message
-        super().__init__(message)
+    # get User items
+    async def getUserItems(self, user: discord.Member, itemname: str):
+        items = await self.database.Select(
+            itemname,
+            user.id,
+            user.guild.id,
+            table="test.inv",
+            condition="items = $1 AND user_id = $2 AND guild_id = $3",
+            columns="items"
+        )
+        if not user:
+            raise NotFound()
+        return items
+
+    # add items to the shop
+    async def addShopItem(self, name: str, cost: int, emoji: str):
+
+        return await self.database.Insert(
+            name,
+            cost,
+            emoji,
+            table="test.shop",
+            columns="items,cost,emoji",
+            values="$1,$2,$3",
+            on_Conflicts="""(items) DO
+            UPDATE SET cost = $2,emoji = $3"""
+        )
+
+    async def removeUserItems(self, user: discord.Member, itemname: str, forced: bool = True):
+        items = await self.GetItemNamed(item=itemname)
+
+        await self.database.Delete(
+            itemname,
+            user.id,
+            user.guild.id,
+            table="test.inv",
+            condition="items = $1 AND user_id = $2 AND guild_id = $3"
+        )
+        if not forced:
+            await self.removeUserCoins(
+                user=user,
+                coins=items[0]['cost']
+            )
+        return items
+
+    async def removeShopItems(self):
+        ...
