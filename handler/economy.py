6d1d678bea193da056e5bd83ea977b7828383019
Green Apple <akshitverma7361@gmail.com>
2022-10-17 00:57:17 +0530

    added methods to manage booster items

--- handler/economy.py
+++ handler/economy.py
@@ -1,18 +1,24 @@
+from __future__ import annotations
+
 """
 economy class for peepbot
 ~~~~~~~~~~~~~~~~~~~
 :copyright: (c) xgreenapple(green apple)
 :license: MIT see LICENSE for more details
 """
+import datetime
 import typing
+from typing import TYPE_CHECKING
 import discord
 
 from handler.database import database
 from handler.errors import (
     UserHasNotEnoughCoins,
     NotEnoughMembers,
-    DataDoesNotExists, NotFound
+    DataDoesNotExists, NotFound, ItemNotFound, BadRequest
 )
+if TYPE_CHECKING:
+    from pepebot import pepebot
 
 
 # Economy base class, some useful methods that we will use later
@@ -153,24 +159,32 @@ class Economy:
     """Get shop Related stuffs"""
 
     # get shop items
-    async def GetShopItems(self):
-        ...
+    async def GetShopItems(self, Column: str = "*"):
+        raw_items = await self.database.Select(
+            table="test.shop",
+            columns=Column,
+            return_everything=True
+        )
+        if raw_items is None:
+            raise NotFound()
+        return raw_items
 
     # return item with its name
-    async def GetItemNamed(self, item: str):
+    async def GetItemNamed(self, item: str, column: str = "items,cost,emoji"):
         item = await self.database.Select(
             item,
             return_everything=True,
             table="test.shop",
             condition="items = $1",
-            columns="items,cost"
+            columns=column
         )
-        if not item:
+        if not item or not len(item):
             raise NotFound("this item doesnt exist")
-        return item
+        print('item ; ',item)
+        return item[0]
 
-    # get User items
-    async def getUserItems(self, user: discord.Member, itemname: str):
+    # get a item from user
+    async def getUserItem(self, user: discord.Member, itemname: str):
         items = await self.database.Select(
             itemname,
             user.id,
@@ -183,19 +197,50 @@ class Economy:
             raise NotFound()
         return items
 
-    # add items to the shop
-    async def addShopItem(self, name: str, cost: int, emoji: str):
+        # get a item from user
 
+    async def getUserItems(self, user: discord.Member):
+        items = await self.database.Select(
+            user.id,
+            user.guild.id,
+            table="test.inv",
+            condition=" user_id = $1 AND guild_id = $2",
+            columns="items"
+        )
+        if items is None or len(items) == 0:
+            raise DataDoesNotExists()
+
+        if not user:
+            raise NotFound()
+        return items
+
+    # add items to the shop
+    async def addShopItem(self, name: str, cost: float, emoji: str, seconds: typing.Optional[int] = None):
+        expired = datetime.timedelta(seconds=seconds) if seconds else None
         return await self.database.Insert(
             name,
             cost,
             emoji,
+            expired,
             table="test.shop",
-            columns="items,cost,emoji",
-            values="$1,$2,$3",
+            columns="items,cost,emoji,expired",
+            values="$1,$2,$3,$4",
             on_Conflicts="""(items) DO
-            UPDATE SET cost = $2,emoji = $3"""
+            UPDATE SET cost = $2,emoji = $3, expired = $4"""
         )
+
+    # add items to user inventory
+    async def addItemtoInv(self, user: discord.Member, itemname: str):
+        item = await self.GetItemNamed(itemname, column='expired,items')
+        await self.database.Insert(
+            user.id,
+            user.guild.id,
+            item['items'],
+            item['expired'] + datetime.datetime.now() if item['expired'] else None,
+            table="test.inv",
+            columns="user_id,guild_id,items,expired",
+            values="$1,$2,$3,$4")
+
 
     async def removeUserItems(self, user: discord.Member, itemname: str, forced: bool = True):
         items = await self.GetItemNamed(item=itemname)
@@ -214,5 +259,104 @@ class Economy:
             )
         return items
 
-    async def removeShopItems(self):
-        ...
+    # remove item from the shop
+    async def removeShopItems(self, itemname: str):
+        # check if item exists in the list if not raise not found error
+        await self.GetItemNamed(item=itemname)
+        await self.database.Delete(
+            itemname.lower(),
+            table="test.shop",
+            condition="items = $1"
+        )
+
+    # buy a item from the shop
+    async def Buyitem(self, user: discord.Member, item_name: str, bot:pepebot):
+        items = await self.GetShopItems(Column="items,cost,emoji")
+        PointBooster = await self.isPoint_booster(item_name=item_name, guild_id=user.guild.id)
+
+        User_Coin = await self.getUserCoins(user)
+        User_inv = await self.getUserItem(user=user, itemname=item_name)
+
+        if User_Coin is None:
+            raise DataDoesNotExists()
+        if User_inv:
+            raise BadRequest("cant buy more than 1 item")
+        try:
+            rawItem = await self.GetItemNamed(
+                item_name.lower(), column='items,cost,emoji,expired')
+            print('raw item :' ,rawItem)
+            cost = rawItem['cost']
+            emoji = rawItem['emoji']
+            expired = rawItem['expired']
+        except NotFound:
+            raise ItemNotFound()
+        threshold = 0
+        if User_Coin < cost:
+            raise UserHasNotEnoughCoins
+        if PointBooster:
+            cost, expired, threshold = await self.AddBooster(user=user)
+        if expired:
+            await bot.taskrunner.ReloadTask()
+        else:
+            await self.addItemtoInv(user=user, itemname=item_name)
+
+        total = await self.removeUserCoins(user=user, coins=cost)
+        return cost, emoji, total, expired, PointBooster, threshold
+
+    async def AddBooster(self, user: discord.Member):
+        """ adds  item to user inventory """
+        data = await self.GetBooster(guild_id=user.guild.id)
+        await self.database.Insert(
+            user.id,
+            user.guild.id,
+            data['item_name'],
+            data['expired'] + datetime.datetime.now() if data['expired'] else None,
+            table="test.inv",
+            columns="user_id,guild_id,items,expired",
+            values="$1,$2,$3,$4")
+        return data['cost'], data['expired'], data['threshold']
+
+    async def GetBooster(self, guild_id: int):
+        data = await self.database.Select(
+            guild_id,
+            table="test.booster",
+            columns="item_name,cost,expired",
+            condition="guild_id = $1"
+        )
+        if not data:
+            raise NotFound('booster for this guild does not exists')
+        return data
+
+    async def isPoint_booster(self, item_name: str, guild_id: int) -> bool:
+        data = await self.database.Select(
+            guild_id,
+            item_name,
+            table="test.booster",
+            columns='item_name',
+            condition='guild_id = $1 AND item_name = $2'
+        )
+        if data:
+            raise True
+        else:
+            return False
+
+    async def UpdateShopBooster(self, guild_id: int, name: str, cost: float,emoji:str):
+        data = await self.GetBooster(guild_id=guild_id)
+        expired: typing.Optional[datetime.timedelta] = data['expired'] if data['expired'] else None
+        total_seconds = expired.total_seconds() if expired else None
+        await self.addShopItem(name=name, seconds=total_seconds, cost=cost, emoji=emoji)
+
+    async def Insert_Booster(self, name: str, guild_id: int, threshold: float, expired: datetime.timedelta):
+        await self.database.Insert(
+            guild_id,
+            name,
+            threshold,
+            expired,
+            table='test.booster',
+            columns='guild_id,item_name,threshold,expired',
+            values='$1,$2,$3,$4',
+            on_Conflicts=
+            """(guild_id) DO
+            UPDATE SET item_name = $2,threshold=$3,expired=$4
+            """
+        )
